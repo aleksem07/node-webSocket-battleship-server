1573869849995da76030df8d699e0080f6ed1cf0
aleksem07 <aleksem07@gmail.com>
2024-02-15 01:46:18 +0300

    feat: add ws.on message

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,32 @@ require('dotenv').config();
 
 wss.on('connection', (ws) => {
   console.log('Client connected');
+
   ws.on('message', (message) => {
-    console.log('Received message:', message.toString());
-    wss.clients.forEach((client) => {
-      client.send(message.toString());
-    });
+    const data = JSON.parse(message.toString());
+
+    if (data.type === 'error') {
+      console.log(data);
+      return;
+    }
+
+    if (data.type === 'reg') {
+      const resp = { type: 'reg', data: '{"name":"222","password":"dfgdfgdfg"}', id: 0 };
+      // wss.clients.forEach((client) => {
+      ws.send(JSON.stringify(resp));
+      // });
+      return;
+    }
+
+    if (data.type === 'create_room') {
+      const resp = { type: 'create_game', data: '{"idGame":"1","idPlayer":"0"}', id: 0 };
+      wss.clients.forEach((client) => {
+        client.send(JSON.stringify(resp));
+      });
+      return;
+    }
+
+    console.log(message.toString());
   });
 
   ws.on('close', () => {
