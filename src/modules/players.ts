68ef5dd11e3fac32f51f0ef1ec5af3a6866a501c
aleksem07 <aleksem07@gmail.com>
2024-02-16 17:47:09 +0300

    feat: update winners

--- src/modules/players.ts
+++ src/modules/players.ts
@@ -2,16 +2,18 @@ export interface IPlayer {
   id: number;
   name: string;
   password: string | number;
+  wins?: number;
 }
 
 export class Players {
   public players: IPlayer[] = [];
 
-  registerPlayer(name: string, password: string): IPlayer {
+  registerPlayer(name: string, password: string, wins = 0): IPlayer {
     const player: IPlayer = {
       id: this.players.length,
       name,
       password,
+      wins,
     };
     this.players.push(player);
     return player;
--- src/websocket/ws.ts
+++ src/websocket/ws.ts
@@ -22,14 +22,30 @@ export class WSHandler {
         let user = this.players.players[data.id];
         // let room = this.rooms.rooms[data.idGame];
 
+        if (!data) {
+          console.error('No data received');
+          return;
+        }
+
         switch (data.type) {
           case 'reg':
             const parsedData = JSON.parse(data.data);
+
+            if (!parsedData.name || !parsedData.password) {
+              console.error('Name and password are required');
+              return;
+            }
+            if (parsedData.name.length < 5 || parsedData.password.length < 5) {
+              console.error('Password must be at least 5 characters long');
+              return;
+            }
+
             const name = parsedData.name;
             const password = parsedData.password;
             user = this.players.registerPlayer(name, password);
-            console.log(user);
-            const response = {
+            this.players.players[user.id] = user;
+
+            const responseRegUser = {
               type: 'reg',
               data: JSON.stringify({
                 name: user.name,
@@ -37,23 +53,41 @@ export class WSHandler {
                 error: false,
                 errorText: '',
               }),
-              id: 0,
+              id: data.id,
             };
             // wss.clients.forEach((client) => {
-            ws.send(JSON.stringify(response));
+            ws.send(JSON.stringify(responseRegUser));
             // });
+
+            const allPlayers = this.players.players.map((player) => ({
+              name: player.name,
+              wins: player.wins,
+            }));
+            const responseUpdateWinners = {
+              type: 'update_winners',
+              data: JSON.stringify(allPlayers),
+              id: data.id,
+            };
+            wss.clients.forEach((client) => {
+              client.send(JSON.stringify(responseUpdateWinners));
+            });
+
+            console.log(this.players.players);
             break;
 
           case 'create_room':
-          // const respRoom = {
-          //   type: 'create_game',
-          //   data: JSON.stringify({ idGame: room.id, idPlayer: user.id }),
-          //   id: 0,
-          // };
-          // wss.clients.forEach((client) => {
-          //   client.send(JSON.stringify(respRoom));
-          // });
-          // break;
+            // const respRoom = {
+            //   type: 'create_game',
+            //   data: JSON.stringify({ idGame: room.id, idPlayer: user.id }),
+            //   id: 0,
+            // };
+            // wss.clients.forEach((client) => {
+            //   client.send(JSON.stringify(respRoom));
+            // });
+            break;
+
+          default:
+            console.error('Unknown message type');
         }
       });
 
